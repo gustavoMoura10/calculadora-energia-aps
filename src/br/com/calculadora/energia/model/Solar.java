1720748028e9d5e6fc68c6b165ff39dbf0a6f62b
gustavoMoura10 <35973820+gustavoMoura10@users.noreply.github.com>
2018-05-10 09:23:03 -0300

    Update Solar.java
    05/10/2018

--- src/br/com/calculadora/energia/model/Solar.java
+++ src/br/com/calculadora/energia/model/Solar.java
@@ -1,32 +1,49 @@
 package br.com.calculadora.energia.model;
 
-public class Solar implements Energia{
+public class Solar implements Energia {
 	private int placas;
 	private Locacao locacao;
-	
+
 	public int getPlacas() {
 		return placas;
 	}
-
 
 	public void setPlacas(int placas) {
 		this.placas = placas;
 	}
 
-
 	public Locacao getLocacao() {
 		return locacao;
 	}
-
 
 	public void setLocacao(Locacao locacao) {
 		this.locacao = locacao;
 	}
 
+	public void valorEnergiaPorRegiao() {
+		switch (this.getLocacao().getRegiaoBrasil()) {
+		case "NORTE":
+			this.quantidadeDePlacas(5.5);
+			break;
+		case "NORDESTE":
+			this.quantidadeDePlacas(5.9);
+			break;
+		case "CENTRO-OESTE":
+			this.quantidadeDePlacas(5.7);
+			break;
+		case "SUDESTE":
+			this.quantidadeDePlacas(5.6);
+			break;
+		case "SUL":
+			this.quantidadeDePlacas(5.2);
+			break;
+		default:
+			this.quantidadeDePlacas(0);
+			break;
+		}
+	}
 
-	@Override
-	public double valorEnergia() {
-		
+	public int quantidadeDePlacas(double d) {
 		return 0;
 	}
 
